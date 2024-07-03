69b5c8b64bf79911cd6611d54d7e2ea41afd8117
BusraEcemSakar <busra.ecem.sakar@gmail.com>
2024-07-03 21:31:17 +0300

    data_preprocessing.R

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -1,21 +1,13 @@
 # Load necessary libraries
 library(dplyr)
-library(survival)
-library(ggplot2)
-library(ggfortify)
-library(randomForestSRC)
-library(epiDisplay)
-library(doMC)
-library(pec)
-library(Rcpp)
 
 # Load data
 BreastCancer <- read.csv("data/BreastCancer.csv")
 
 # Data preprocessing
-BreastCancer$DogumKontrolHapiKull <- factor(BreastCancer$DogumKontrolHapiKull, levels = c("1", "2"), labels = c("Evet", "Hayır"))
-BreastCancer$TümörTani <- factor(BreastCancer$TümörTani, levels = c("1", "2"), labels = c("Evet", "Hayır"))
-BreastCancer$NeoadjuvanKull <- factor(BreastCancer$NeoadjuvanKull, levels = c("1", "2"), labels = c("Evet", "Hayır"))
+BreastCancer$BirthControlPillUse <- factor(BreastCancer$BirthControlPillUse, levels = c("1", "2"), labels = c("Yes", "No"))
+BreastCancer$TumorDiagnosis <- factor(BreastCancer$TumorDiagnosis, levels = c("1", "2"), labels = c("Early", "Late"))
+BreastCancer$NeoadjuvantUse <- factor(BreastCancer$NeoadjuvantUse, levels = c("1", "2"), labels = c("Yes", "No"))
 
 # Save processed data
 write.csv(BreastCancer, "data/ProcessedBreastCancer.csv", row.names = FALSE)
